a9589e3ddf65d639f0404a20f6a3468d7dd739da
Panfeng <liupanfeng@zuhebox.com>
2016-07-05 22:36:36 +0800

    暂存，2016-7-5

---
+++ src/views/Home/Home.js
@@ -0,0 +1,17 @@
+import React, { Component, PropTypes } from 'react';
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className='container text-center'>
+        <h1>Welcome to daizhe.xyz</h1>
+      </div>
+    )
+  }
+}
+
+export default Home;
